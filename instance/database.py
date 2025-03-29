cb6e8a31de41b8bde6ddb86cab3aae769514183d
muoimeo <bvnminh6a01@gmail.com>
2025-03-29 16:55:01 +0700

    Delete duplicated files, changed color theme, removed payment in navbar, linked payment in book_ride and in edit profile

--- instance/database.py
+++
@@ -1,2 +0,0 @@
-# instance/database.py
-SQLALCHEMY_DATABASE_URI = 'mysql://user:MinhDZ3009@localhost/rideshare'
--- src/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from .book_ride import book_ride_bp
-from .cancel_ride import cancel_ride_bp
-from .discounts import discounts_bp
-from .errors import errors_bp
-from .home import home_bp
-from .payment import payment_bp
-from .users import users_bp
